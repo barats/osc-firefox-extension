36d1122a1b98392a7f6ec916604ac32ea33f3cd6
巴拉迪维 <baratsemet@gmail.com>
2020-05-09 15:20:12 +0800

    从 Chrome 浏览器插件移植

---
+++ README.md
@@ -0,0 +1,18 @@
+# OSCHINA Firefox Extension
+
+#### 概要
+开源中国 (oschina.net) 火狐浏览器插件，聚合开源中国每日最新资讯、推荐精品博客、最新收录开源软件等优质内容。
+
+![latest-snapshot](https://oscimg.oschina.net/oscnet/up-e62572addce0fb4d932031e138b3587a1aa.JPEG)
+
+#### 请通过 debug 方式安装此插件
+
+#### 如何参与本项目贡献
+如果你觉得这项目挺有意思，亦或想要吐槽，欢迎通过以下方式提交 PR
+
+1.  fork 本仓库
+2.  新建 feature_xxx 分支或 bugfix_xxx 分支
+3.  修复发现的 bug 或新增功能并提交代码
+4.  新建 Pull Request 等待审核
+
+#### 最后，欢迎吐槽！ Enjoy. 
---
+++ js/contants.js
@@ -0,0 +1,42 @@
+//全部资讯
+const RSS_NEWS = 'https://www.oschina.net/news/rss';
+
+//资讯：其他分类
+const RSS_NEWS_PREFIX = 'https://www.oschina.net/news/rss?show=';
+
+//问答前缀
+const RSS_QUESTIONS_PREFIX = 'https://www.oschina.net/question/rss?cid=';
+
+//开源软件：最新收录
+const RSS_PROJECTS = 'https://www.oschina.net/project/rss';
+
+//开源软件：最新推荐
+const RSS_RECOMM_PROJECTS = 'https://www.oschina.net/project/rss?show=more';
+
+//最新推荐（全部）
+const RSS_RECOMM_BLOGS = 'https://www.oschina.net/blog/rss';
+
+//博客前缀
+const RSS_RECOMM_BLOGS_CATEGORY_PREFIX = 'https://www.oschina.net/blog/rss?cid=';
+
+//UTM_SOURCE 配置
+const UTM_SOURCE_STR = 'utm_source=oschina-firefox-extension';
+
+//Gitee 仓库链接
+const GITEE_REPO_URL = 'https://gitee.com/barat/osc-firefox-extension?' + UTM_SOURCE_STR;
+
+//搜索前缀
+const OSC_SEARCH_PREFIX = 'https://www.oschina.net/search?scope=all&q=';
+
+//书签文件夹名
+const OSC_BOOKMARKS_NAME = "开源中国";
+
+function stripHtml(html) {
+    var doc = new DOMParser().parseFromString(html, 'text/html');
+    return doc.body.textContent || '';
+}
+
+function stripXml(xml) {
+    var doc = new DOMParser().parseFromString(xml, 'text/xml');
+    return doc.body.textContent || '';
+}
